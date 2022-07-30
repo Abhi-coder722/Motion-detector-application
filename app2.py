6c99270bcfeeeb1fe8e0851958c7dac49c97ec45
Abhi-coder722 <109688594+Abhi-coder722@users.noreply.github.com>
2022-07-30 19:23:35 +0530

    Updated some bugs
    Changed the contour length of the application

--- app2.py
+++ app2.py
@@ -1,72 +1,69 @@
+import cv2, time, pandas
 from datetime import datetime
-from re import I
-import cv2 , time,pandas
 
-first_frame=None
+first_frame = None
+status_list = [None,None]
+times = []
+df = pandas.DataFrame(columns=["Start","End"])
 
-status_list=[None,None]
+video=cv2.VideoCapture(0)
 
-vdo=cv2.VideoCapture(0, cv2.CAP_DSHOW)
-
-# for recording the time using the date time method wherever the object isn't present 
-times=[]
-df=pandas.DataFrame(columns=["start","end"])
+video.read()
+time.sleep(1)
 
 while True:
-
-    status=0
-    check,frame=vdo.read()
-
-    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-
-    gray=cv2.GaussianBlur(gray,(21,21),0)
+    check, frame = video.read()
+    status = 0
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray,(21,21), 0)
 
     if first_frame is None:
-        first_frame= gray
+        first_frame = gray
         continue
 
-    delta_frame=cv2.absdiff(first_frame,gray)
-    thres_frame=cv2.threshold(delta_frame,20,255,cv2.THRESH_BINARY)[1]
-    thres_frame=cv2.dilate(thres_frame,None,iterations=2)
+    delta_frame = cv2.absdiff(first_frame,gray)
+    thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+    thresh_frame = cv2.dilate(thresh_frame, None, iterations=2)
 
-    (cnts,_)=cv2.findContours(thres_frame.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    (cnts,_) = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in cnts:
-        if cv2.contourArea(contour)<10000:
+        if cv2.contourArea(contour) < 5000:
             continue
-        #if the contour is equal or bigger than 1000 then we will create a rectangle ..
-        status=1
-        (x,y,w,h)=cv2.boundingRect(contour)
-        # take the coordinates and then create a recatngle over there 
-        cv2.rectangle(frame,(x,y),((x+w),(y+h)),(0,255,0),3)
+        status = 1
+
+        (x, y, w, h) = cv2.boundingRect(contour)
+        reci = cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0), 3)
     status_list.append(status)
+
     status_list=status_list[-2:]
-    if status_list[-1]==1 and status_list[-2]==0:
+
+
+    if status_list[-1] == 1 and status_list[-2] == 0:
         times.append(datetime.now())
-    if status_list[-1]==0 and status_list[-2]==1:
+    if status_list[-1] == 0 and status_list[-2] == 1:
         times.append(datetime.now())
 
 
-    cv2.imshow("t's me ",gray)
-    cv2.imshow("Delta",delta_frame)
-    cv2.imshow("Threshold frame ",thres_frame)
-    cv2.imshow("Color Frame",frame)
+    cv2.imshow("Gray Frame", gray)
+    cv2.imshow("Delta Frame", delta_frame)
+    cv2.imshow("Threshold Frame", thresh_frame)
+    cv2.imshow("Color Frame", frame)
 
-    key = cv2.waitKey(1)
+    key=cv2.waitKey(1)
 
-    if key==ord('q'):
-        if status==1:
+    if key == ord('q'):
+        if status == 1:
             times.append(datetime.now())
         break
 
 print(status_list)
 print(times)
 
-for i in range(0,len(times),2):
-    df=df.append({"start":(times[i]),"end":(times[i+1])},ignore_index=True)
+for i in range(0,len(times), 2):
+    df=df.append({"Start":times[i], "End":times[i+1]}, ignore_index=True)
 
 df.to_csv("Times.csv")
 
-cv2.destroyAllWindows()
-vdo.release()
-
+video.release()
+cv2.destroyAllWindows
